0cbdc42bc6a263b6b3c3cfe2dd1677f1d86cba17
Mahdi Rahman <35085896+mahdirahman1@users.noreply.github.com>
2022-04-21 00:45:45 -0400

    login page work

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,16 +4,24 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+	uri: "http://localhost:4000/graphql",
+	cache: new InMemoryCache(),
+});
 
 const root = ReactDOM.createRoot(document.getElementById(
 	"root"
 ) as HTMLElement);
 root.render(
 	<React.StrictMode>
-		<ChakraProvider>
-			<BrowserRouter>
-				<App />
-			</BrowserRouter>
-		</ChakraProvider>
+		<ApolloProvider client={client}>
+			<ChakraProvider>
+				<BrowserRouter>
+					<App />
+				</BrowserRouter>
+			</ChakraProvider>
+		</ApolloProvider>
 	</React.StrictMode>
 );
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -11,11 +11,42 @@ import {
 	Heading,
 	Text,
 	useColorModeValue,
+	FormErrorMessage,
 } from "@chakra-ui/react";
+import { useQuery, gql, useLazyQuery } from "@apollo/client";
+import { useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
+
+const LOGIN_USER = gql`
+	query Query($email: String!, $password: String) {
+		login(email: $email, password: $password) {
+			userId
+			token
+		}
+	}
+`;
 
 const Login = () => {
 	const navigate = useNavigate();
+	const emailRef: any = useRef();
+	const passRef: any = useRef();
+
+	interface opvars {
+		email: string;
+		password: string;
+	}
+
+	const [login, { loading, error, data }] = useLazyQuery<opvars>(LOGIN_USER);
+
+	useEffect(
+		() => {
+			if (data && !loading) {
+				navigate("/events")
+			}
+		},
+		[data, loading, navigate]
+	);
+
 	return (
 		<Flex
 			minH={"100vh"}
@@ -23,11 +54,15 @@ const Login = () => {
 			justify={"center"}
 			bg={useColorModeValue("gray.100", "gray.800")}
 		>
-			<Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
+			<Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6} >
 				<Stack align={"center"}>
 					<Heading fontSize={"4xl"}>Sign in to your account</Heading>
 					<Text fontSize={"lg"} color={"gray.600"}>
-						to enjoy all the fun <Link color={"blue.400"} onClick={() => navigate("/events")}>events</Link> ✌️
+						to enjoy all the fun{" "}
+						<Link color={"blue.400"} onClick={() => navigate("/events")}>
+							events
+						</Link>{" "}
+						✌️
 					</Text>
 				</Stack>
 				<Box
@@ -35,17 +70,18 @@ const Login = () => {
 					bg={useColorModeValue("white", "gray.700")}
 					boxShadow={"lg"}
 					p={8}
+					border={error ? "2px solid red" : ''}
 				>
 					<Stack spacing={4}>
 						<FormControl id="email">
 							<FormLabel>Username</FormLabel>
-							<Input type="email" isRequired/>
+							<Input type="text" ref={emailRef} isRequired />
 						</FormControl>
 						<FormControl id="password">
 							<FormLabel>Password</FormLabel>
-							<Input type="password" />
+							<Input type="password" ref={passRef} />
 						</FormControl>
-						<FormControl>
+						<FormControl isInvalid>
 							<Button
 								w="100%"
 								bg={"blue.400"}
@@ -53,19 +89,30 @@ const Login = () => {
 								_hover={{
 									bg: "blue.500",
 								}}
+								disabled={loading}
+								type="submit"
+								onClick={() =>
+									login({
+										variables: {
+											email: emailRef.current.value,
+											password: passRef.current.value,
+										},
+									})
+								}
 							>
 								Sign in
 							</Button>
+							<FormErrorMessage justifyContent="center">{error?.message}</FormErrorMessage>
 						</FormControl>
 					</Stack>
 					<Stack pt={6}>
-							<Text align={"center"}>
-								Need an account?{" "}
-								<Link color={"blue.400"} onClick={() => navigate("/signup")}>
-									Signup
-								</Link>
-							</Text>
-						</Stack>
+						<Text align={"center"}>
+							Need an account?{" "}
+							<Link color={"blue.400"} onClick={() => navigate("/signup")}>
+								Signup
+							</Link>
+						</Text>
+					</Stack>
 				</Box>
 			</Stack>
 		</Flex>
